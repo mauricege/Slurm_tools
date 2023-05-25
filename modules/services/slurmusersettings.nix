e37b209c4268515cfd2a86a082ae4a3be93ba0b3
Maurice Gerczuk <maurice.gerczuk@uni-a.de>
2023-05-25 14:39:29 +0200

    Update slurm-accounts service.

--- modules/services/slurmusersettings.nix
+++ modules/services/slurmusersettings.nix
@@ -1,6 +1,10 @@
-{ lib, config, pkgs, ... }:
-with lib;
-let
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+with lib; let
   cfg = config.services.slurmusersettings;
   groupOpts = {...}: {
     options = {
@@ -60,63 +64,61 @@ let
   '';
   doNotCreateUserString = user: "NEWUSER:${user}:dontcreate";
   doNotCreateUsers = concatStringsSep "\n" (map doNotCreateUserString cfg.dontCreate);
-  accountConfFile = with builtins; pkgs.writeTextDir "accounts.conf" ''
-  ###
-  ### Slurm accounts for cluster eihw-compute
-  ###
-  ### Syntax of the file is:
-  ### account:parent:FairShare:Description
-  ${(concatStringsSep "\n" (attrValues (mapAttrs groupToAccountConf cfg.groups)))}
-  '';
-  userConfFile = with builtins; pkgs.writeTextDir "user_settings.conf"
-  ''
-  #
-  # This file defines *user* fairshare and limit values for UNIX groups or usernames.
-  # Use this to assign values to all users within a UNIX primary group.
-  # Specific usernames may also be configured to override the default or group values.
-  #
+  accountConfFile = with builtins;
+    pkgs.writeTextDir "accounts.conf" ''
+      ###
+      ### Slurm accounts for cluster eihw-compute
+      ###
+      ### Syntax of the file is:
+      ### account:parent:FairShare:Description
+      ${(concatStringsSep "\n" (attrValues (mapAttrs groupToAccountConf cfg.groups)))}
+    '';
+  userConfFile = with builtins;
+    pkgs.writeTextDir "user_settings.conf"
+    ''
+      #
+      # This file defines *user* fairshare and limit values for UNIX groups or usernames.
+      # Use this to assign values to all users within a UNIX primary group.
+      # Specific usernames may also be configured to override the default or group values.
+      #
 
-  #
-  # List syntax (fields are separated by ":"):
-  #
-  # [DEFAULT/UNIX_group/username]:[Type]:value
-  #
-  # Type examples: fairshare/GrpTRES/GrpTRESRunMins/QOS/DefaultQOS/MaxJobs/MaxSubmitJobs/MaxJobsAccrue/GrpJobsAccrue
-  # Default settings for users without mapped UNIX group
-  ${defaultToUserConf cfg.default}
+      #
+      # List syntax (fields are separated by ":"):
+      #
+      # [DEFAULT/UNIX_group/username]:[Type]:value
+      #
+      # Type examples: fairshare/GrpTRES/GrpTRESRunMins/QOS/DefaultQOS/MaxJobs/MaxSubmitJobs/MaxJobsAccrue/GrpJobsAccrue
+      # Default settings for users without mapped UNIX group
+      ${defaultToUserConf cfg.default}
 
-  # Settings for users with mapped accounts
-  ${(concatStringsSep "\n\n" (attrValues (mapAttrs groupToUserConf cfg.groups)))}
+      # Settings for users with mapped accounts
+      ${(concatStringsSep "\n\n" (attrValues (mapAttrs groupToUserConf cfg.groups)))}
 
-  # Do not create accounts for these users
-  ${doNotCreateUsers}
-  '';
-
+      # Do not create accounts for these users
+      ${doNotCreateUsers}
+    '';
 in {
-
   options = {
     services.slurmusersettings = {
       enable = mkEnableOption "slurmusersettings service";
-      autoSync = mkOption {
+      autoUpdate = mkOption {
         type = types.bool;
         default = false;
       };
       groups = mkOption {
         type = types.attrsOf (types.submodule groupOpts);
-        default = { };
+        default = {};
         example = {
-          DEFAULT = { fairshare=1; };
+          DEFAULT = {fairshare = 1;};
         };
-        description =
-          ''
-            User groups and their corresponding limits.
-          '';
+        description = ''
+          User groups and their corresponding limits.
+        '';
       };
       default = mkOption {
-        type = removeAttrs (types.submodule groupOpts) [ "parent" ];
-        default = { };
-        description = 
-        ''
+        type = removeAttrs (types.submodule groupOpts) ["parent"];
+        default = {};
+        description = ''
           Default slurm settings for users without mapped group.
         '';
       };
@@ -129,14 +131,33 @@ in {
   };
 
   config = let
-      wrappedUpdateAccounts = pkgs.writeShellScriptBin "updateslurmaccounts" ''
-      exec env SLURM_CONF=${config.services.slurm.etcSlurm}/slurm.conf env SLURM_USER_SETTINGS=${config.services.slurm.etcSlurm}/user_settings.conf env SLURM_ACCOUNT_SETTINGS=${config.services.slurm.etcSlurm}/accounts.conf ${pkgs.slurm-tools.updateslurmaccounts}/bin/updateslurmaccounts
-      '';
-    in
-    mkIf cfg.enable {
-      environment.systemPackages = [
-        wrappedUpdateAccounts
-      ];
-      services.slurm.extraConfigPaths = [ accountConfFile userConfFile ];
-    };
+    wrappedUpdateAccounts = pkgs.writeShellScriptBin "updateslurmaccounts" ''
+      exec env SLURM_CONF=${config.services.slurm.etcSlurm}/slurm.conf env SLURM_USER_SETTINGS=${config.services.slurm.etcSlurm}/user_settings.conf env SLURM_ACCOUNT_SETTINGS=${config.services.slurm.etcSlurm}/accounts.conf ${pkgs.slurm-tools.updateslurmaccounts}/bin/updateslurmaccounts $@
+    '';
+  in
+    mkIf cfg.enable (
+      mkMerge [
+        {
+          environment.systemPackages = [
+            wrappedUpdateAccounts
+          ];
+          services.slurm.extraConfigPaths = [accountConfFile userConfFile];
+        }
+        (mkIf cfg.autoUpdate {
+          systemd.services = {
+            update-slurmaccounts = {
+              description = "Automatically update slurmaccounts for system-level users.";
+              requires = ["slurmdbd.service" "nslcd.service"];
+              after = ["slurmdbd.service" "nslcd.service"];
+              wantedBy = ["multi-user.target"];
+              script = "${wrappedUpdateAccounts}/bin/updateslurmaccounts -y";
+              path = with pkgs; [nss_pam_ldapd glibc];
+            };
+            # updateslurmaccounts.text = ''
+            #   ${wrappedUpdateAccounts}/bin/updateslurmaccounts -n
+            # '';
+          };
+        })
+      ]
+    );
 }
